84ab87f4140c98127749a2f51523ab9b12065a29
alex <alex.palermo.45@gmail.com>
2023-01-10 01:07:42 -0500

    updated install scripts

--- installs/xppen.sh
+++ installs/xppen.sh
@@ -1,5 +1,5 @@
 cd ~/Downloads
 curl -L https://www.xp-pen.com/download/file/id/1936/pid/440/ext/gz.html -o xppen.tar.gz
 tar -xf xppen.tar.gz
-cd xppen
+cd xp-pen-pentablet-3.2.3.220323-1.x86_64
 sudo bash install.sh
--- setup_debian.sh
+++ setup_debian.sh
@@ -1,5 +1,12 @@
 sudo apt-get install vim curl cmake pip
 
+echo "********** ANACONDA **********"
+if [ ! -d /home/alex/anaconda3/ ]; then
 bash ./installs/anaconda.sh
+fi
+
+echo "********** XPPEN **************"
 bash ./installs/xppen.sh
+
+echo "********** VIM PLUG ***********"
 bash ./installs/install_vim_plug.sh
